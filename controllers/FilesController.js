a0bf3e2c777fc23ab445813c4c8f3dcb08b50b53
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-10-16 12:47:19 +0300

    Task 8 : File data

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,7 +1,8 @@
 import { ObjectId } from 'mongodb';
 import { v4 as uuidv4 } from 'uuid';
-import { promises as fs } from 'fs';
+import { promises as fs, existsSync } from 'fs';
 import path from 'path';
+import mime from 'mime-types';
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
@@ -186,5 +187,43 @@ export default class FilesController {
     file.value.id = file.value._id;
     delete file.value._id;
     return res.status(200).send(file.value);
+  }
+
+  static async getFile(req, res) {
+    const token = req.headers['x-token'];
+    const fileId = req.params.id;
+    const { size } = req.query;
+
+    const file = await dbClient.dataClient.db().collection('files').findOne({ _id: ObjectId(fileId) });
+    if (!file) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    if (!file.isPublic) {
+      if (!token) {
+        return res.status(404).json({ error: 'Not found' });
+      }
+      const userId = await redisClient.get(`auth_${token}`);
+      if (!userId || userId !== file.userId.toString()) {
+        return res.status(404).json({ error: 'Not found' });
+      }
+    }
+
+    if (file.type === 'folder') {
+      return res.status(400).json({ error: "A folder doesn't have content" });
+    }
+
+    let filePath = file.localPath;
+    if (size && ['100', '250', '500'].includes(size)) {
+      filePath = `${file.localPath}_${size}`;
+    }
+
+    if (!existsSync(filePath)) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    const mimeType = mime.lookup(file.name);
+    const data = await fs.readFile(filePath);
+    return res.header('Content-Type', mimeType).status(200).send(data);
   }
 }
